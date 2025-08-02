7c3aea57af2f903b3579bf283af3d17b0dfd74ce
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-08-02 11:46:05 +0300

    Add SignUp cubit with Firebase authentication and error handling

---
+++ lib/cubits/sign_up_cubit/sign_up_state.dart
@@ -0,0 +1,13 @@
+abstract class SignUpState {}
+
+class SignUpInitial extends SignUpState {}
+
+class SignUpLoading extends SignUpState {}
+
+class SignUpSuccess extends SignUpState {}
+
+class SignUpFailure extends SignUpState {
+  final String errorMessage;
+
+  SignUpFailure({required this.errorMessage});
+}
