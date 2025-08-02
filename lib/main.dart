c7bebb2e3ec717394953cd2746eeb3341d1db1eb
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-08-02 11:38:01 +0300

    Add SimpleBlocObserver integration to main.dart for enhanced Bloc state monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:chat_app/cubits/languge_cubit/language_state.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/generated/l10n.dart';
 import 'package:chat_app/routes/app_route.dart';
+import 'package:chat_app/simple_bloc_observer.dart';
 import 'package:chat_app/views/welcome_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ void main() async {
   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
   final savedLang = sharedPreferences.getString('language') ?? 'en';
 
+  Bloc.observer = SimpleBlocObserver();
   runApp(ChatApp(savedLang: savedLang));
 }
 
